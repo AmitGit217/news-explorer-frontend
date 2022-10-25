0dd81196d68fd95855d77c4cd6eedfdfbf286c44
Amit Bar-Gil <amit217@yandex.com>
2022-10-25 16:42:46 +0300

    feat: inital header jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,13 +2,17 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Main from "../Main/Main";
 import SavedNews from "../SavedNews/SavedNews";
+import Header from "../Header/Header";
 import "./App.css";
 
 export default function App() {
     return (
-        <Routes>
-            <Route path='/' element={<Main />} />
-            <Route path='/saved-news' element={<SavedNews />} />
-        </Routes>
+        <>
+            <Header />
+            <Routes>
+                <Route path='/' element={<Main />} />
+                <Route path='/saved-news' element={<SavedNews />} />
+            </Routes>
+        </>
     );
 }
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,5 +2,17 @@ import React from "react";
 import "./Header.css";
 
 export default function Header() {
-    return <p>Hello World !</p>;
+    return (
+        <header className='header'>
+            <h1 className='header__title'>NewsExplorer</h1>
+            <nav className='header__nav'>
+                <button className='header__home-button' type='button'>
+                    Home
+                </button>
+                <button className='header__signin-button' type='button'>
+                    Sign in
+                </button>
+            </nav>
+        </header>
+    );
 }
