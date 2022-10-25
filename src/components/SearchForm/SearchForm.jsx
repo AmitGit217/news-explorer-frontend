678cf194f6a86b37f85cfb49bac1f3b6a498ce60
Amit Bar-Gil <amit217@yandex.com>
2022-10-25 18:37:57 +0300

    style: position search form items

--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -4,13 +4,15 @@ import "./SearchForm.css";
 export default function SearchForm() {
     return (
         <div className='search-form'>
-            <h2 className='search-form__title'>
-                What's going on in the world?
-            </h2>
-            <h3 className='search-form__subtitle'>
-                Find the latest news on any topic and save them in your personal
-                account.
-            </h3>
+            <div className='search-form__text'>
+                <h2 className='search-form__title'>
+                    What's going on in the world?
+                </h2>
+                <h3 className='search-form__subtitle'>
+                    Find the latest news on any topic and save them in your
+                    personal account.
+                </h3>
+            </div>
             <div className='search-form__search'>
                 <input className='search-form__search-text' type='text' />
                 <button className='search-form__search-button' type='submit' />
