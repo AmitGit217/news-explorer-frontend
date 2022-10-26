2869d096e21339ef9a27a5272eb7ef5c18434f3c
Amit Bar-Gil <amit217@yandex.com>
2022-10-26 14:55:46 +0300

    feat: about jsx markup

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,6 +1,23 @@
 import React from "react";
 import "./About.css";
+import about from "../../images/about.svg";
 
 export default function About() {
-    return <p>Hello World !</p>;
+    return (
+        <section className='about'>
+            <img className='about__image' src={about} alt='author' />
+            <div className='about__main'>
+                <h6 className='about__title'>About the author</h6>
+                <p className='about__info'>
+                    This block describes the project author. Here you should
+                    indicate your name, what you do, and which development
+                    technologies you know.
+                </p>
+                <p className='about__info'>
+                    You can also talk about your experience with Practicum, what
+                    you learned there, and how you can help potential customers.
+                </p>
+            </div>
+        </section>
+    );
 }
