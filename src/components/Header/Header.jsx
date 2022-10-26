ba56628a8b2f07205cf37799fe51b0fd0d78d456
Amit Bar-Gil <amit217@yandex.com>
2022-10-26 18:21:17 +0300

    fix: conditional header need to be more simple

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,28 +1,20 @@
 import React from "react";
 import "./Header.css";
 import { Link } from "react-router-dom";
-import { useState } from "react";
 
 export default function Header() {
-    const [activeNav, setActiveNav] = useState("home");
     return (
         <header className='header'>
             <h1 className='header__title'>NewsExplorer</h1>
             <nav className='header__nav'>
                 <Link
-                    onClick={() => setActiveNav("home")}
                     to={"/"}
-                    className={`header__button header__button-nav ${
-                        activeNav === "home" && "header__button_active"
-                    }`}>
+                    className='header__button header__button-nav header__button_active'>
                     Home
                 </Link>
                 <Link
-                    onClick={() => setActiveNav("saved-news")}
                     to={"/saved-news"}
-                    className={`header__button header__button-nav ${
-                        activeNav === "saved-news" && "header__button_active"
-                    }`}>
+                    className='header__button header__button-nav'>
                     Saved articles
                 </Link>
                 <button
