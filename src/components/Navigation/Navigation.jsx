1b322707d9bb033dd28dc765eb31e92866e0955b
Amit Bar-Gil <amit217@yandex.com>
2022-10-30 12:10:55 +0200

    safe: home for 425px

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -6,7 +6,6 @@ import { useNavigate } from "react-router-dom";
 
 export default function Navigation() {
     const navigate = useNavigate();
-
     const { setPopup } = useContext(PopupContext);
     const { isMobileNavOpen, setMobileNav } = useContext(NavContext);
 
@@ -14,12 +13,14 @@ export default function Navigation() {
         setPopup(true);
         setMobileNav(false);
     };
-
     const backHome = () => {
         navigate("/");
         setMobileNav(false);
     };
-
+    const goToSavedArticlesPage = () => {
+        setMobileNav(false);
+        navigate("/saved-news");
+    };
     return (
         <div
             className={`popup-nav__overlay ${
@@ -35,9 +36,16 @@ export default function Navigation() {
                     />
                 </div>
                 <div className='popup-nav__bottom'>
-                    <p className='popup-nav__home-link' onClick={backHome}>
-                        Home
-                    </p>
+                    <div className='popup-nav__links'>
+                        <p className='popup-nav__link' onClick={backHome}>
+                            Home
+                        </p>
+                        <p
+                            className='popup-nav__link'
+                            onClick={goToSavedArticlesPage}>
+                            Saved Articles
+                        </p>
+                    </div>
                     <button
                         className='popup-nav__signin'
                         onClick={openFormPopup}>
