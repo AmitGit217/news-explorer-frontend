faf1392b64b0ba7226e762b0d05791db595c435b
Amit Bar-Gil <amit217@yandex.com>
2022-10-31 15:25:47 +0200

    fix: posotion popup

--- src/components/PopupWithForm/PopupWithForm.jsx
+++ src/components/PopupWithForm/PopupWithForm.jsx
@@ -2,22 +2,29 @@ import React, { useContext } from "react";
 import { PopupContext } from "../../contexts/PopupContext";
 import "./PopupWithForm.css";
 import { useCloseFromEsc } from "../../utils/functions";
+import { RegisterContext } from "../../contexts/RegisterContext";
 
 export default function PopupWithForm({ children }) {
+    const { setRegister } = useContext(RegisterContext);
     const { isPopupWithFormOpen, setPopupWithForm } = useContext(PopupContext);
+    const closePopupWithForm = () => {
+        setPopupWithForm(false);
+        setRegister(true);
+    };
+
     useCloseFromEsc(setPopupWithForm);
 
     return (
         <div
             className={`popup ${isPopupWithFormOpen && "popup_show"}`}
             onClick={(e) =>
-                e.target === e.currentTarget && setPopupWithForm(false)
+                e.target === e.currentTarget && closePopupWithForm()
             }>
             <div className='popup__container'>
                 <button
                     className='popup__exit'
                     type='button'
-                    onClick={() => setPopupWithForm(false)}
+                    onClick={closePopupWithForm}
                 />
                 {children}
             </div>
