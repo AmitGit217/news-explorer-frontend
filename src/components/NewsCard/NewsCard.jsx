3d78906092f18c43a757c5bd1f11b934f1051263
Amit Bar-Gil <amit217@yandex.com>
2022-11-03 15:03:51 +0200

    feat: disable on isLoggedIn

--- src/components/NewsCard/NewsCard.jsx
+++ src/components/NewsCard/NewsCard.jsx
@@ -14,11 +14,13 @@ export default function NewsCard({
     // Temporary state visualization before API connection
     const [tempIsSaved, setTempIsSaved] = useState(false);
     const { isLoggedIn } = useStore().currentUser;
+    const realDate = new Date(date);
+
     return (
         <article className='news-card'>
             <img className='news-card__image' src={image} alt={title} />
             <div className='news-card__main'>
-                <p className='news-card__date'>{date}</p>
+                <p className='news-card__date'>{realDate.toDateString()}</p>
                 <h2 className='news-card__title'>{title}</h2>
                 <p className='news-card__text'>{text}</p>
                 <p className='news-card__source'>{source}</p>
@@ -27,6 +29,7 @@ export default function NewsCard({
                 <button
                     onClick={() => setTempIsSaved(!tempIsSaved)}
                     type='button'
+                    disabled={isLoggedIn ? false : true}
                     className={` ${
                         tempIsSaved
                             ? "news-card__archive-image_saved"
